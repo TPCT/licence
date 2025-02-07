e1921096b67b9195e0dd5c6760d5aefd71704d5f
tpct <eslamjackie@outlook.com>
2025-02-07 03:24:04 +0200

    adding start date and end date

--- resources/views/Applications/index.blade.php
+++ resources/views/Applications/index.blade.php
@@ -22,7 +22,7 @@
                                         @if($user->active)
                                             <div class="d-flex justify-content-center align-items-center flex-column">
                                                 <span class="badge bg-warning">{{$application->pivot->start_date}} - {{$application->pivot->end_date}}</span>
-                                                <span class="alert text-center alert-success p-1 mt-1">Remaining: {{\Carbon\Carbon::parse($application->pivot->end_date)->diffInDays(\Carbon\Carbon::today(), false)}} Days</span>
+                                                <span class="alert text-center alert-success p-1 mt-1">Remaining: {{\Carbon\Carbon::parse($application->pivot->end_date)->diffForHumans(\Carbon\Carbon::today())}} Days</span>
                                             </div>
                                         @else
                                             <div class="d-flex align-items-center mb-2">
